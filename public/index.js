2ef10baad35784e918b598d0c69b82fead9fcc75
sai krishna mohan gupta singamsetty <saikrishnamohanguptasingamsetty@sais-MacBook-Air.local>
2021-03-08 21:41:57 +0530

    Two step form without responsive

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
 var Form1 = document.getElementById("Form1");
 var Form2 = document.getElementById("Form2");
-var Form3 = document.getElementById("Form3");
+// var Form3 = document.getElementById("Form3");
 
 var Next1 = document.getElementById("Next1");
-var Next2 = document.getElementById("Next2");
+// var Next2 = document.getElementById("Next2");
 var Back1 = document.getElementById("Back1");
-var Back2 = document.getElementById("Back2");
+// var Back2 = document.getElementById("Back2");
 
 var Submit = document.getElementById("submit");
 
@@ -31,7 +31,7 @@ Next1.onclick = function(){
     if(ValidForm1()){
         Form1.style.left = "-550px";
         Form2.style.left = "40px";
-        progress.style.width = "320px";
+        progress.style.width = "480px";
     }
     else{
         alert("Fill all mandatory blanks with *");
@@ -40,27 +40,27 @@ Next1.onclick = function(){
 Back1.onclick = function(){
     Form1.style.left = "40px";
     Form2.style.left = "550px";
-    progress.style.width = "160px";
+    progress.style.width = "240px";
 }
 
-Next2.onclick = function(){
-    if(ValidForm2()){
-        Form2.style.left = "-550px";
-        Form3.style.left = "40px";
-        progress.style.width = "480px";
-    }
-    else{
-        alert("Please answer all questions!");
-    }
-}
-Back2.onclick = function(){
-    Form2.style.left = "40px";
-    Form3.style.left = "550px";
-    progress.style.width = "320px";
-}
+// Next2.onclick = function(){
+//     if(ValidForm2()){
+//         Form2.style.left = "-550px";
+//         Form3.style.left = "40px";
+//         progress.style.width = "480px";
+//     }
+//     else{
+//         alert("Please answer all questions!");
+//     }
+// }
+// Back2.onclick = function(){
+//     Form2.style.left = "40px";
+//     Form3.style.left = "550px";
+//     progress.style.width = "320px";
+// }
 
 Submit.onclick = function(){
-    if(ValidForm3()){
+    if(ValidForm2()){
         console.log(data);
         fetch("/feedback",{
             method: 'POST',
